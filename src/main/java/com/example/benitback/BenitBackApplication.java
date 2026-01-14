f1f0e7c6f7fed1e40fac44724781083319c51204
kjkim <kjkim@osci.kr>
2026-01-14 11:20:34 +0900

    CO10101013_테스트커밋47

--- src/main/java/com/example/benitback/BenitBackApplication.java
+++ src/main/java/com/example/benitback/BenitBackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BenitBackApplication {
-    // 테스트46
+    // 테스트47
     public static void main(String[] args) {
         SpringApplication.run(BenitBackApplication.class, args);
     }
